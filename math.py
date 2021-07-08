da33a4aed3a6ec6f3bcb85850fd00f8634c6615e
keshav kabra <46310949+keshav-kabra@users.noreply.github.com>
2021-07-08 14:28:58 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#addition 1
+def add(x,y):
+    return x+y
+
+#subtract 1
+def subtract(x,y):
+    return x-y
+
+#multiply header 1
+def multiply(x,y):
+    return x*y
+
+#divide header 1
+def divide(x,y):
+    return x*1/y
